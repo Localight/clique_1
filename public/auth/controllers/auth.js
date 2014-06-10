2b38d1715fb821283f84b5f33a0f30c043866dca
alexander <ag.saldivar@gmail.com>
2014-06-10 11:39:47 -0700

    shipping angular controller

--- public/auth/controllers/auth.js
+++ public/auth/controllers/auth.js
@@ -12,25 +12,25 @@ angular.module('mean.controllers.login', [])
                     email: $scope.user.email,
                     password: $scope.user.password
                 })
-                    .success(function(response) {
-                        // authentication OK
-                        $scope.loginError = 0;
-                        $rootScope.user = response.user;
-                        $rootScope.$emit('loggedin');
-                        if (response.redirect) {
-                            if (window.location.href === response.redirect) {
-                                //This is so an admin user will get full admin page
-                                window.location.reload();
-                            } else {
-                                window.location = response.redirect;
-                            }
+                .success(function(response) {
+                    // authentication OK
+                    $scope.loginError = 0;
+                    $rootScope.user = response.user;
+                    $rootScope.$emit('loggedin');
+                    if (response.redirect) {
+                        if (window.location.href === response.redirect) {
+                            //This is so an admin user will get full admin page
+                            window.location.reload();
                         } else {
-                            $location.url('/');
+                            window.location = response.redirect;
                         }
-                    })
-                    .error(function() {
-                        $scope.loginerror = 'Authentication failed.';
-                    });
+                    } else {
+                        $location.url('/');
+                    }
+                })
+                .error(function() {
+                    $scope.loginerror = 'Authentication failed.';
+                });
             };
         }
     ])
